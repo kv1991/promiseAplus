1d3fdc3c12ac034a41f450e03d0200ba2d4d7eca
Kaiwen Tian <kaiwen.tian@wishlife.com>
2021-12-15 10:59:23 +0800

    feat:  return a promise in the then function and added catching error.

--- promiseAplus.js
+++ promiseAplus.js
@@ -41,10 +41,12 @@ function MyPromise(executor) {
   }
 }
 
-// #1 then方法: 由于then方法可以链式调用，所以是实例方法，而且规范中的api是MyPromise.then(onFulfilled, onRejected), 所以基本的架子如下:
-// 规范里要求：
+// #1 then方法: 由于then方法可以链式调用,所以是实例方法,而且规范中的api是MyPromise.then(onFulfilled, onRejected), 所以基本的架子如下:
+// 规范里要求: 
 // 1. 先检查onFulfilled以及onRejected是不是函数
-// 2. 如果不是函数就忽略他们，所谓忽略就是如果是onFulfilled那么就返回value，如果是onRejected那么就返回reason，因为onRejected属于错误分支，那么应该throw an error.
+// 2. 如果不是函数就忽略他们,所谓忽略就是如果是onFulfilled那么就返回value,如果是onRejected那么就返回reason,因为onRejected属于错误分支,那么应该throw an error.
+// #4 根据规范,then的返回值必须是promise,这样才能实现链式调用,规范中还定义了不同情况如何处理,下面是简单的几种情况:
+// #4-1: 如果onFulfilled或者onRejected抛出一个异常，则promise2必须拒绝执行，并返回拒因
 // #1
 MyPromise.prototype.then = function (onFulfilled, onRejected) {
   let realOnFulfilled = onFulfilled;
@@ -61,23 +63,53 @@ MyPromise.prototype.then = function (onFulfilled, onRejected) {
     }
   }
   
-  // #2 当我们使用Promise的时候，如果执行成功, 那么就会执行onFulfilled, 如果失败了就会执行onRejected
-  // 所以我们检查status，如果是fulfilled，那么就调用onFulfilled，如果是rejected，那我们调用onRejected.
+  // #2 当我们使用Promise的时候,如果执行成功, 那么就会执行onFulfilled, 如果失败了就会执行onRejected
+  // 所以我们检查status,如果是fulfilled,那么就调用onFulfilled,如果是rejected,那我们调用onRejected.
   
-  // #3 这里有个问题就是我们初始化promise可能类似：new Promise(fn).then(onFulfilled, onRejected),这里一初始化后，就可以调用onFulfilled/onRejected，显然是不对的，要等到初始化成功，
-  // 并且判断status状态，如果是PENDING，那么就应该将他们存起来，等到fn有了结论，resolve/reject的时候再来调用对应的代码;
-  // 还有可能多个then伴随多个onFulfilled或者是onRejected，所以可以先存起来，等resolve或者是rejected的时候拿出来一一执行一遍;
+  // #3 这里有个问题就是我们初始化promise可能类似: new Promise(fn).then(onFulfilled, onRejected),这里一初始化后,就可以调用onFulfilled/onRejected,显然是不对的,要等到初始化成功,
+  // 并且判断status状态,如果是PENDING,那么就应该将他们存起来,等到fn有了结论,resolve/reject的时候再来调用对应的代码;
+  // 还有可能多个then伴随多个onFulfilled或者是onRejected,所以可以先存起来,等resolve或者是rejected的时候拿出来一一执行一遍;
   // #3
+  let that = this;
   if (this.status === PENDING) {
-    this.onFulfilledCallbacks.push(realOnFulfilled);
-    this.onRejectedCallbacks.push(realOnRejected);
+    // #4-1
+    return new MyPromise((resolve, reject) => {
+      that.onFulfilledCallbacks.push(() => {
+        try {
+          realOnFulfilled(that.value)
+        } catch (err) {
+          reject(err);
+        }
+      });
+      that.onRejectedCallbacks.push(() => {
+        try {
+          realOnRejected(that.reason);
+        } catch (err) {
+          reject(err);
+        }
+      });
+    });
   }
   // #2
   if (this.status === FULFILLED) {
-    onFulfilled(this.value);
+    // #4-1
+    return new MyPromise((resolve, rejected) => {
+      try {
+        realOnFulfilled(this.value);
+      } catch (err) {
+        rejected(err);
+      }
+    });
   }
   if (this.status === REJECTED) {
-    onFulfilled(this.reason);
+    // #4-1
+    return new MyPromise((resolve, reject) => {
+      try {
+        realOnRejected(this.reason);
+      } catch (err) {
+        reject(err);
+      }
+    })
   }
 
 }
